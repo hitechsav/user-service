80a4710f511633901ab4d663dddf74f5ab2470a3
asahu108 <49301889+asahu108@users.noreply.github.com>
2019-04-05 12:59:51 +0530

    Intitial Setup files

---
+++ src/main/java/org/entertheheavens/service/user/main/UserServiceBootStartUp.java
@@ -0,0 +1,8 @@
+package org.entertheheavens.service.user.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceBootStartUp {
+
+}
---
+++ src/test/java/org/entertheheavens/test/controller/UserControllerTest.java
@@ -0,0 +1,5 @@
+package org.entertheheavens.test.controller;
+
+public class UserControllerTest {
+
+}
