69e3e536f9eee83e3fbb0002a3685cac2046d00d
asahu108 <49301889+asahu108@users.noreply.github.com>
2019-04-09 19:20:23 +0530

    eureka server

--- src/main/java/org/entertheheavens/service/user/main/UserServiceBootStartUp.java
+++ src/main/java/org/entertheheavens/service/user/main/UserServiceBootStartUp.java
@@ -1,8 +1,15 @@
 package org.entertheheavens.service.user.main;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class UserServiceBootStartUp {
+	
+	public static void main(String[] a) {
+		SpringApplication.run(UserServiceBootStartUp.class, a);
+	}
 
 }
